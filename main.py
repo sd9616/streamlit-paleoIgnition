29c78f4580c5fa6f6bbb912be95c99545eb17f27
sd9616 <sd96@rice.edu>
2024-07-16 16:01:46 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+from streamlit_option_menu import option_menu
+
+st.title('Lightning App')
+
+# with st.sidebar:
+#   selected = option_menu(
+#     menu_title = "Main Menu",
+#     options = ["Home","Lightning Plotter","Contact"],
+#     icons = ["house","lightning","envelope"],
+#     menu_icon = "cast",
+#     default_index = 0,
+
+#   )
