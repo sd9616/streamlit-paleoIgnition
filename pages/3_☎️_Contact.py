837db81995c81ae9cb0a69bd6c975ef933778883
sd9616 <sd96@rice.edu>
2024-07-16 23:31:23 +0530

    added logo, edited homepage, contact page

---
+++ pages/3_☎️_Contact.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write("# Contact Info")
+# st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    PaleoIgnition is an app built for 
+
+    **👈 Select a demo from the dropdown on the left** to see some examples
+    of what PaleoIgnition can do!
+
+""")
