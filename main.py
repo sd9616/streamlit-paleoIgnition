2a4eef9f33b752c05c63aa3c943bf4977c1999a5
sd9616 <sd96@rice.edu>
2024-07-18 14:51:20 +0530

    updated main

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ with col1:
         PaleoIgnition is an app built to provide paleofire practitioners with a paleolightning reconstruction for their study sites. 
         
         * Head to the __Lightning Time Series Plotter__ page on the left side bar to create a reconstruction of any site
+        * Head to the __Lightning Map Plotter__ page to create heat maps for any point in time 
         * Find out more about the datasets used on the __Dataset Info + References__ page
         
         """)
